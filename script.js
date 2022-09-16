ca322f72d520e373f36e0b430005873f602f1857
Joshua Caleb Bolito <bolitojoshuacaleb@gmail.com>
2022-09-16 19:12:13 +0800

    Unfinished initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+}
+
+function addBookToLibrary() {
+	let newBook = new Book();
+}
+
