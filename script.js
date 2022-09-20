e3eb40640d8364a7f934a6d042afca790e8ae7fe
Joshua Caleb Bolito <bolitojoshuacaleb@gmail.com>
2022-09-20 17:47:55 +0800

    functionality has barely started

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+// -----------------
+
+const form = document.querySelector('form');
+const closeForm = document.querySelector('.close');
+const addBookButton = document.querySelector('.addBook');
+const saveBookButton = document.querySelector('saveBook');
+let inputTitle = document.querySelector('#title');
+let inputAuthor = document.querySelector('#author');
+let inputPages = document.querySelector('#pages');
+let inputRead = document.querySelector('#read');
+
+
+//OPENS FORM
+addBookButton.addEventListener('click', function () {
+	form.classList.remove('hidden');
+});
+
+
+//CLOSES FORM
+closeForm.addEventListener('click', function () {
+	form.classList.add('hidden');
+});
+
+// OBJECTS
 let myLibrary = [];
 
 class Book {
@@ -9,8 +34,22 @@ class Book {
 	}
 }
 
-function addBookToLibrary() {
-	let newBook = new Book();
+// ADD BOOK INFORMATION
+function clearInputs() {
+	inputTitle.value = '';
+	inputAuthor.value = '';
+	inputPages.value = '';
+	inputRead.value = '';
 }
-// -----------------
+
+function addBookToLibrary() {
+	form.classList.add('hidden');
+	let newBook = new Book();
+	newBook.title = inputTitle.value;
+	newBook.author = inputAuthor.value;
+	newBook.pages = inputPages.value;
+	newBook.read = inputRead.value;
+	myLibrary.push(newBook);
+	clearInputs();
+}
 
